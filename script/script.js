0edee0b847d424950a9a01997f5c19ba69b6a1d3
ronlfk <123903091+ronlfk@users.noreply.github.com>
2023-03-07 16:14:48 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-var NumberOfWords = 30
+var NumberOfWords = 10
 var words = new BuildArray(NumberOfWords)
 
 // Use the following variables to 
@@ -12,27 +12,7 @@ words[6] = "https://ckk.ai/OTzKwYj"
 words[7] = "https://ckk.ai/Sl88YPkwf" 
 words[8] = "https://ckk.ai/bReJSGH" 
 words[9] = "https://ckk.ai/ZF8B" 
-words[10] = "https://ckk.ai/bhRwKjyT" 
-words[11] = "https://ckk.ai/92WtcB" 
-words[12] = "https://ckk.ai/kMYqeXaeFBB" 
-words[13] = "https://ckk.ai/P9T1q3k" 
-words[14] = "https://ckk.ai/9zllwD" 
-words[15] = "https://ckk.ai/MZ35n" 
-words[16] = "https://ckk.ai/RhzRN8UpZXF" 
-words[17] = "https://ckk.ai/EsRbo" 
-words[18] = "https://ckk.ai/bHTMTv" 
-words[19] = "https://ckk.ai/N3Ca8xowKRp" 
-words[20] = "https://ckk.ai/RYESnqpBN8"
-words[21] = "https://ckk.ai/Ilb5e29F"
-words[22] = "https://ckk.ai/JkV5Wjup9s" 
-words[23] = "https://ckk.ai/Q8CBRiPQD" 
-words[24] = "https://ckk.ai/kpDCIm2yj" 
-words[25] = "https://ckk.ai/iB6lXVCL3ue" 
-words[26] = "https://ckk.ai/wzUmPiD" 
-words[27] = "https://ckk.ai/4z9Gu6zFDL" 
-words[28] = "https://ckk.ai/UitNs" 
-words[29] = "https://ckk.ai/80vUvISo" 
-words[30] = "https://ckk.ai/myjv6GvrBas" 
+words[10] = "https://ckk.ai/bhRwKjyT"  
 
 function BuildArray(size){
 this.length = size
