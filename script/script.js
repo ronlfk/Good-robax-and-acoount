1250fa27cdc6b5523ae0c02e3c2df33fab21a378
ronlfk <123903091+ronlfk@users.noreply.github.com>
2023-03-07 17:04:42 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,16 +3,16 @@ var words = new BuildArray(NumberOfWords)
 
 // Use the following variables to 
 // define your random words:
-words[1] = "" 
-words[2] = "" 
-words[3] = "" 
-words[4] = "" 
-words[5] = "" 
-words[6] = "" 
-words[7] = "" 
-words[8] = "" 
-words[9] = "" 
-words[10] = ""  
+words[1] = "https://tii.la/75njf596LwD" 
+words[2] = "https://tii.la/UJZPZS" 
+words[3] = "https://tii.la/KICSPs1" 
+words[4] = "https://tii.la/mOFofPjf" 
+words[5] = "https://tii.la/gPVtbV" 
+words[6] = "https://tii.la/PpD6aG" 
+words[7] = "https://tii.la/M4EwU" 
+words[8] = "https://tii.la/BUUHNRSsVM" 
+words[9] = "https://tii.la/K0Ip8LA7g2" 
+words[10] = "https://tii.la/R5XfCiu8Kv"  
 
 function BuildArray(size){
 this.length = size
